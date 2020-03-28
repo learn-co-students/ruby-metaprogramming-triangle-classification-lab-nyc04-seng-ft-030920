46aee50a9986f514cf76c70472ea5d382f670b50
GaryZCLau <garyzclau@gmail.com>
2020-03-27 23:52:08 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
 
   def bottomhalflearnworks
     @a = 
-    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || (@side1 + @side2 < @side3) || (@side3 + @side2 < @side1) || (@side1 + @side3 <= @side2)
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || (@side1 + @side2 <= @side3) || (@side3 + @side2 <= @side1) || (@side1 + @side3 <= @side2)
       false
     else
       true
