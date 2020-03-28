7d6109873f284a47ae1c7231a55a51d41fbb261e
GaryZCLau <garyzclau@gmail.com>
2020-03-27 23:51:29 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require 'pry'
 class Triangle
   # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    bottomhalflearnworks
+
+    if @side1 == @side2 && @side1 == @side3 && @side2 == @side3 #&& @side1 != 0 || @side2 != 0 || @side3 != 0
+      :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles
+    elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+      :scalene
+    end
+
+  end
+
+  def bottomhalflearnworks
+    @a = 
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || (@side1 + @side2 < @side3) || (@side3 + @side2 < @side1) || (@side1 + @side3 <= @side2)
+      false
+    else
+      true
+    end
+
+    if @a != true
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
